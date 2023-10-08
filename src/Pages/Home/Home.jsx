2261cb72394e0a1b0dfe432b0760eccd5a6cb125
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-10-08 11:17:30 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
